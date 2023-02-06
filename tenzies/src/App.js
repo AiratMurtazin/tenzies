ca05360de7e2047fde8990fffedbece5cffc1055
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-02-06 15:36:32 +0300

    implement timer

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './index.css';
 import Die from './Die.js';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import Confetti from 'react-confetti';
 
@@ -9,6 +9,25 @@ function App() {
 	const [dice, setDice] = React.useState(allNewDice());
 	const [tenzies, setTenzies] = React.useState(false);
 	const [rolls, setRolls] = React.useState(0);
+
+	//* Stopwatch
+
+	const [time, setTime] = React.useState(0);
+	const [start, setStart] = React.useState(false);
+
+	useEffect(() => {
+		let interval = null;
+
+		if (start) {
+			interval = setInterval(() => {
+				setTime(prevTime => prevTime + 10);
+			}, 10);
+		} else {
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval);
+	}, [start]);
 
 	React.useEffect(() => {
 		const allHeld = dice.every(die => die.isHeld);
@@ -83,6 +102,26 @@ function App() {
 				Number of rolls: <span id='rolls-number'>{rolls}</span>
 			</div>
 			<div className='dice-container'>{diceElements}</div>
+			<div className='stopWatch'>
+				<h4>Time</h4>
+				<h3>
+					<span>{'0' + ((time / 60000) % 60).slice(-2)}</span>
+					<span>{'0' + ((time / 1000) % 60).slice(-2)}</span>
+					<span>{'0' + ((time / 10) % 1000).slice(-2)}</span>
+				</h3>
+				<div>
+					<button onClick={() => setStart(true)}>Start</button>
+					<button onClick={() => setStart(false)}>Stop</button>
+					<button
+						onClick={() => {
+							setTime(0);
+							setStart(false);
+						}}
+					>
+						Reset
+					</button>
+				</div>
+			</div>
 			<button className='roll-dice' onClick={rollDice}>
 				{tenzies ? 'New Game' : 'Roll'}
 			</button>
